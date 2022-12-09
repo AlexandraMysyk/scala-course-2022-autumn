b834d352b5713ad66802b3bdae0803b5b92507d2
alechka03 <99685662+alechka03@users.noreply.github.com>
2022-12-09 18:09:18 +0200

    Revert "Didn't check the work also, but seems to be correct. Waiting for comm…"

--- src/main/scala/karazin/scala/users/group/week3/Homework.scala
+++ src/main/scala/karazin/scala/users/group/week3/Homework.scala
@@ -1,72 +1,50 @@
 package karazin.scala.users.group.week3
 
 object Homework:
-
+  
   // Peano numbers
   abstract class Nat:
     def isZero: Boolean
     def predecessor: Nat
-    def successor: Nat =
-      Nat+1
-
+    def successor: Nat = ???
+    
     infix def + (that: Nat): Nat
-
+    
     infix def - (that: Nat): Nat
-
+    
     // Optional task
     def toInt: Int
-
+    
     // Optional task
-    def fromInt(int: Int) =
-      if int<=0 then throw new Exception("Not a natural number")
-      else int
-
+    def fromInt(int: Int) = ???
+  
     override def toString: String = s"Nat($predecessor)"
-
-  type Zero = Zero.type
+  
+  type Zero = Zero.type 
   object Zero extends Nat:
     def isZero: Boolean = true
     def predecessor: Nat = throw new Exception("0 doesn't have a predecessor")
-
-    infix def +(that: Nat): Nat =
-      that
-
-    infix def -(that: Nat): Nat =
-      throw new Exception("This is not a natural number")
-
+    
+    infix def +(that: Nat): Nat = ???
+    
+    infix def -(that: Nat): Nat = ???
+    
     // Optional task
-    def toInt: Int =
-      0
+    def toInt: Int = ???
 
     override def toString: String = "Zero"
-    override def equals(obj: Any): Boolean =
-      if obj==0 then true
-      else false
+    override def equals(obj: Any): Boolean = ???
 
   class Succ(n: Nat) extends Nat:
     def isZero: Boolean = false
-    def predecessor: Nat = this
-
-    infix def +(that: Nat): Nat =
-      def f(sum:Nat,n:Nat):Nat=
-        if n==0 then sum
-        else f(sum.successor,n.predecessor)
-      f(this,that)
-
-    infix def -(that: Nat): Nat =
-      def f(dif: Nat, n: Nat): Nat =
-        if n == 0 then dif
-        else f(dif.predecessor, n.predecessor)
-      if (that>this) then throw Exception("This is not a natural number")
-      else f(this,that)
-
+    def predecessor: Nat = ???
+    
+    infix def +(that: Nat): Nat = ???
+    
+    infix def -(that: Nat): Nat = ???
+    
     // Optional task
-    def toInt: Int =
-      this
+    def toInt: Int = ???
 
-
-    override def equals(obj: Any): Boolean =
-      if obj.getClass != Nat then false
-      else obj == this
-
+    override def equals(obj: Any): Boolean = ???
 
--- src/test/scala/karazin/scala/users/group/week3/HomeworkSpecification.scala
+++ src/test/scala/karazin/scala/users/group/week3/HomeworkSpecification.scala
@@ -7,62 +7,27 @@ import Homework._
 
 object HomeworkSpecification extends Properties("Homework"):
 
-  import arbitraries.{given Arbitrary[Int], given Arbitrary[Nat]}
+  ???
 
 end HomeworkSpecification
 
 
 object ZeroSpecification extends Properties("Zero"):
 
-  property ("isZero")=forAll{(n: Zero)=>
-    n.isZero()
-  }
-  property("addition")=forAll{(n:Nat)=>
-    this+n==n
-  }
-  property("toInt")=forAll{(n:Zero)=>
-    n.toInt==0
-  }
-  property("equals")=forAll{(n:Any)=>
-    this.equals(n)==(if n==0 then true else false )
-  }
+  ???
 
 end ZeroSpecification
 
 
 object SuccSpecification extends Properties("Succ"):
 
-  property("isZero")=forAll{()=>
-    this.isZero()==false
-  }
-  property("predecessor")=forAll{()=>
-    this.predecessor()==this
-  }
-  property("addition")=forAll { (n: Nat) =>
-    (this + n).getClass==Nat
-    (this+n) - n == this
-  }
-  property("subtraction")=forAll{(n:Nat)=>
-    if(this>n) then ((this-n).getClass==Nat) && ((this-n)+n==this)
-  }
-  property("toInt")=forAll{
-    (this.toInt()).getClass()==Int
-  }
-  property("equals")=forAll{(n:Nat)=>
-    this.equals(n)==(this==n)
-  }
+  ???
 
 end SuccSpecification
 
 object NatSpecification extends Properties("Nat"):
-  properties("addition")=forAll {
-    Nat1 + Nat2 == Nat2 + Nat1
-  }
-  properties("subtraction")=forAll{
-    if Nat1>Nat2 then (Nat1-Nat2).getClass==Nat
-    else throw Exception("Not a natural number")
-  }
-  properties("equals")=forAll{
-    Nat1.equals(Nat2)==(Nat1==Nat2)
-  }
+ 
+  // Nat1 + Nat2 => ... 
+
 end NatSpecification
+  
