4d4bdef0feb12635c6166b8bbe986fe663194128
Aleksandra Mysyk <aleksandra100854@gmail.com>
2022-12-10 12:19:01 +0200

    week-3

--- src/main/scala/karazin/scala/users/group/week3/Homework.scala
+++ src/main/scala/karazin/scala/users/group/week3/Homework.scala
@@ -1,31 +1,37 @@
 package karazin.scala.users.group.week3
 
+import scala.annotation.tailrec
+
 object Homework:
 
   // Peano numbers
   abstract class Nat:
     def isZero: Boolean
+
     def predecessor: Nat
+
     def successor: Nat =
-      Nat+1
+      Nat + 1
 
-    infix def + (that: Nat): Nat
+    infix def +(that: Nat): Nat
 
-    infix def - (that: Nat): Nat
+    infix def -(that: Nat): Nat
 
     // Optional task
     def toInt: Int
 
     // Optional task
-    def fromInt(int: Int) =
-      if int<=0 then throw new Exception("Not a natural number")
+    def fromInt(int: Int): Int =
+      if int <= 0 then throw new Exception("Not a natural number")
       else int
 
     override def toString: String = s"Nat($predecessor)"
 
   type Zero = Zero.type
-  object Zero extends Nat:
+
+  object Zero extends Nat :
     def isZero: Boolean = true
+
     def predecessor: Nat = throw new Exception("0 doesn't have a predecessor")
 
     infix def +(that: Nat): Nat =
@@ -39,26 +45,32 @@ object Homework:
       0
 
     override def toString: String = "Zero"
+
     override def equals(obj: Any): Boolean =
-      if obj==0 then true
+      if obj == 0 then true
       else false
 
-  class Succ(n: Nat) extends Nat:
+  class Succ(n: Nat) extends Nat :
     def isZero: Boolean = false
+
     def predecessor: Nat = this
 
     infix def +(that: Nat): Nat =
-      def f(sum:Nat,n:Nat):Nat=
-        if n==0 then sum
-        else f(sum.successor,n.predecessor)
-      f(this,that)
+      @tailrec
+      def f(sum: Nat, n: Nat): Nat =
+        if n == 0 then sum
+        else f(sum.successor, n.predecessor)
+
+      f(this, that)
 
     infix def -(that: Nat): Nat =
+      @tailrec
       def f(dif: Nat, n: Nat): Nat =
         if n == 0 then dif
         else f(dif.predecessor, n.predecessor)
-      if (that>this) then throw Exception("This is not a natural number")
-      else f(this,that)
+
+      if that > this then throw Exception("This is not a natural number")
+      else f(this, that)
 
     // Optional task
     def toInt: Int =
@@ -68,5 +80,3 @@ object Homework:
     override def equals(obj: Any): Boolean =
       if obj.getClass != Nat then false
       else obj == this
-
-
